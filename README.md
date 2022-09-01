8e93b77d36ba0898bba17c1be9f52fa8437e4842
otavioabreu27 <otavio.abreu96@gmail.com>
2022-09-01 17:36:09 -0300

    repository_deploy

---
+++ README.md
@@ -0,0 +1,15 @@
+## Instrução de Instalação 
+
+#### Criação do ambiente virtual
+Quando clonar o repositório criar um ambiente virtual, para isso, abra o cmd, navegue até a pasta flask_test e utilize o seguinte código:
+    python -m venv venv
+
+#### Inicialização do ambiente virtual
+Após a criação, navegue até a pasta Scripts dentro da recém criada pasta venv e execute o arquivo activate.bat (QUALQUER DÚVIDA ME ACIONA)
+
+#### Instalando pacotes pip
+Com o ambiente virtual rodando será necessário realizar a instalação de alguns pacotes. Esses pacotes estão discriminados no arquivo "requirements.txt" e portanto para a instalação pip acontecer, navegue até a pasta venv no cmd e digite o código a seguir:
+    pip install -r requirements.txt
+
+#### O arquivo "app.py"
+Dentro do arquivo app.py tem informações importantíssimas sobre algumas ferramentas do código, leia atentamente os comentários e em caso de dúvida entre em contato, bons estudos ;-).
---
+++ app.py
@@ -0,0 +1,41 @@
+# Imports das bibliotecas Flask e mysqldb
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# Configuração inicial do banco de dados
+# *IMPORTANTE*
+# As credenciais de banco podem precisar ser alteradas
+# de computador em computador
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MySQL_PASSWORD'] = 'fatec' 
+app.config['MYSQL_DB'] = 'Flask'
+
+# Instanciando um objeto que herda a classe my sql do mysqldb
+mysql = MySQL(app)
+
+# Instanciando o cursor mysql
+cursor = mysql.connection.cursor()
+
+# Este comando executa uma ação no banco usando linguagem dml ou ddl
+#   cursor.execute(''' CREATE TABLE table_name(field1, field2...) ''')
+
+# Para salvar as alterações feitas no banco
+#   mysql.connection.commit()
+
+# Fechando o cursor
+#   cursor.close()
+
+# Ouando a rota da requisiçao for localhost:5000/ que é a pardrão
+# é chamado o método render_template que carrega uma página html
+# que no flask é chamada de template
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# O __name__ por padrão é main, portanto quando satisfazer essa função
+# a aplicação é rodada
+if __name__ == "__main__":
+    app.run()
